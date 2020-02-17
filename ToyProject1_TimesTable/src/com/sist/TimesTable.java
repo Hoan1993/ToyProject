38b28ed32010e5f5aac76d453309a282ef0d5227
sist <sist@DESKTOP-3J6RDRE>
2020-02-17 16:22:57 +0900

    누가 printTimesTable(int number) 메서드를 구현해주세요.

---
+++ ToyProject1_TimesTable/src/com/sist/TimesTable.java
@@ -0,0 +1,43 @@
+package com.sist;
+
+import java.util.Scanner;
+
+public class TimesTable {
+
+
+	
+	public void commandProcessor() {
+		
+		System.out.println("******구구단 프로그램*******");
+		Scanner kb;
+		while(true) {
+			System.out.print("$ ");
+			kb = new Scanner(System.in);
+			String number = kb.next();
+			if(number.equalsIgnoreCase("exit")) {
+				break;
+			} else {
+				printTimesTable(Integer.parseInt(number));
+			}
+			
+			
+			
+		}
+		System.out.println("******구구단 프로그램 종료*******");
+		kb.close();
+		
+		
+	}
+	
+	private void printTimesTable(int number) {
+		
+		
+	}
+
+	public static void main(String[] args) {
+		TimesTable app = new TimesTable();
+		app.commandProcessor();
+
+	}
+
+}
